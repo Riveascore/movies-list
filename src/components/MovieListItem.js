4144dc810579285f6833d2aeb9dbdcbd52102253
Chris Aga <chrisaga.dev@gmail.com>
2020-08-29 13:34:16 -0500

    Set up movies list to use movies data

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -6,19 +6,18 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import '../stories/movie_list_item.css';
 
-export const MovieListItem = ({ title, image, overview, ...props }) => {
+export const MovieListItem = ({ title, uri, overview, ...props }) => {
   // const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <div>
       <h1
         className="storybook-movie-list-item"
-        {...props}
       >
         {title}
       </h1>
       <img
-        src={""}
-        alt=""
+        src={uri}
+        alt={title}
       />
       <p>
         {overview}
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -5,15 +5,16 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import '../stories/movie_list.css';
+import movies from '../data/movies';
 
 
 import MovieListItem from './MovieListItem';
 
-export const MovieList = ({ movies, ...props }) => {
+export const MovieList = ({ ...props }) => {
   // const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <div>
-      {movies.map(movie => <MovieListItem key={movie.id} {...movie} />)}
+      {movies.map(movie => <MovieListItem key={movie.title} {...movie} />)}
     </div>
   );
 };
